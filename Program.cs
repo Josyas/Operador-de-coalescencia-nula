e57a9b03cad9a131a2903339e0972b59be9ba8fe
Josias <87546094+Josyas@users.noreply.github.com>
2021-10-13 06:35:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Operador_de_coalescência_nula {
+    class Program {
+        static void Main(string[] args) {
+
+
+            double? x = null;
+            double? y = 10;
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
